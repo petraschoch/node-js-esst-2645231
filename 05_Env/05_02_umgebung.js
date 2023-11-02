1f01e6ebc3ddfb0705923511d0268ec181e8927c
petraschoch <116625401+petraschoch@users.noreply.github.com>
2023-11-02 13:55:26 +0000

    Pending changes exported from your codespace

--- 05_Env/05_02_umgebung.js
+++ 05_Env/05_02_umgebung.js
@@ -6,7 +6,7 @@ const serverPort = process.env.MYPORT;
 const name = process.env.MYNAME;
 
 const server = http.createServer((request, response) => {
-  response.write(`Hi ${name}, ich laufe auf Port` + serverPort);
+  response.write(`Hi ${name}, ich laufe auf Port ` + serverPort);
   response.end();
 });
 server.listen(serverPort);
